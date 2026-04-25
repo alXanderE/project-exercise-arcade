9276065a3de0cc36a7a93b0965e4251f10283fe0
Excederal <mdpalmer639@gmail.com>
2026-04-25 13:33:42 -0700

    Delete server-template directory

--- server-template/src/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export async function connectToDatabase(uri) {
-  if (!uri) {
-    throw new Error("MONGODB_URI is required.");
-  }
-
-  await mongoose.connect(uri);
-}
